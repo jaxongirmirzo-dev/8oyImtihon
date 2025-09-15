6f0151dcec5b094e7bed9fb9b5fa054095b1519c
jaxongirmirzo <jaxongirmirzo@gmail.com>
2025-09-15 16:18:18 +0430

    Yangi kodlar qo'shildi

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 function MainLayout() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 }
 
